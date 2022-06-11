69691939364dee80aca9c0bd6fd02e783187b8b5
JakubChorzepa <jakubchorzepa@gmail.com>
2022-06-11 19:06:43 +0200

    Added navbar files and added navbar to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,6 +3,7 @@ import GlobalStyle from '../styles/globalStyles';
 import SEO from './SEO';
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
+import Navbar from './Navbar/Navbar.component';
 
 type Props = {
   title?: string,
@@ -16,7 +17,10 @@ const Layout = ({ title, description, children }: Props) => {
     <ThemeProvider theme={theme}>
       <SEO title={title} description={description} />
       <GlobalStyle />
-      <>{children}</>
+      <>
+        <Navbar />
+        {children}
+      </>
     </ThemeProvider>
   )
 };
---
+++ src/components/Navbar/Navbar.component.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { 
+    NavbarWrapper
+
+  } from './Navbar.styles';
+
+const Navbar = () => {
+  return (
+    <NavbarWrapper>
+      <p>Hello</p>
+      <p>Hello</p>
+    </NavbarWrapper>
+  )
+}
+
+export default Navbar;
